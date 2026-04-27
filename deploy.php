8569a3d148e41b0c5f84910e46890380bbe71fa6
nathanjwatkins <nathanjwatkins@gmail.com>
2026-04-27 21:37:51 +0100

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,6 +1,5 @@
 <?php
-define('GITHUB_TOKEN',   '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');
-define('GITHUB_RAW',     'https://raw.githubusercontent.com/nathanjwatkins/CollectorsVault/main/');
+define('GITHUB_TOKEN',   'ghp_8xHq6W90STUu34Oo0Oon813DaR1HA81Nzpqq');define('GITHUB_RAW',     'https://raw.githubusercontent.com/nathanjwatkins/CollectorsVault/main/');
 define('WEBHOOK_SECRET', 'cv_deploy_2025_nate');
 
 $payload   = file_get_contents('php://input');
